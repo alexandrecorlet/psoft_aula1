7c4892cdf903e788bbedbcd1a30d91a90f9d0731
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-10-05 10:58:06 -0300

    Add Produto Repository

---
+++ src/repository/ProdutoRepository.java
@@ -0,0 +1,27 @@
+package repository;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
+
+import data.Produto;
+
+public class ProdutoRepository {
+
+    private Map<String, Produto> produtos;
+
+    public ProdutoRepository() {
+        produtos = new HashMap<>();
+    }
+
+    public String add(Produto p) {
+        String id = p.getId();
+        produtos.put(id, p);
+        return id;
+    }
+
+    public Collection<Produto> getAll() {
+        return produtos.values();
+    }
+
+}
