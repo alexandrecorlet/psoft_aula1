a18fc672b6bbb45582903373018483114c69d9ba
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-10-05 10:29:59 -0300

    Add equals, hashcode, toString and getters to Product

--- src/data/Produto.java
+++ src/data/Produto.java
@@ -1,6 +1,7 @@
 package data;
 
-import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Produto {
 
@@ -10,14 +11,52 @@ public class Produto {
 
     private String fabricante;
 
-    private Date dataFab;
-
-    private Date dataVal;
+    private final String id;
 
     public Produto(String nome, String fabricante, double preco) {
         this.nome = nome;
         this.fabricante = fabricante;
         this.preco = preco;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Produto: " + nome + '\n'
+                + "ID: " + id + '\n'
+                + "Fabricante: " + fabricante + '\n'
+                + "Preco: R$ " + String.format("%.2f", preco) + '\n';
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
 }
