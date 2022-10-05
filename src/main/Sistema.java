f7c5f74acf49b827fdf6ac5b1b2314405f90d726
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-10-05 11:33:30 -0300

    Update Sistema

--- src/main/Sistema.java
+++ src/main/Sistema.java
@@ -2,12 +2,28 @@ package main;
 
 import data.Produto;
 import data.Lote;
+import facade.Facade;
+
+import java.util.Date;
 
 public class Sistema {
 
     public static void main(String[] args) {
-        Produto p = new Produto("Bike", "Caloi", 1000.00);
-        Lote lote = new Lote(p, 1000);
+        Facade fac = new Facade();
+
+        // Produto
+        Produto dell = fac.criarProduto("Dell XPS", "Dell", 5000.00d);
+        Produto macPro = fac.criarProduto("MacBook Pro 2020", "Apple", 20000.00d);
+        for (Produto p: fac.listarProdutos())
+            System.out.println(p.toString());
+
+        System.out.println();
+
+        // Lote
+        fac.criarLote(dell, 10000, null);
+        fac.criarLote(macPro, 100, null);
+        for (Lote l : fac.listarLotes())
+            System.out.println(l.toString() + '\n');
     }
 
 }
